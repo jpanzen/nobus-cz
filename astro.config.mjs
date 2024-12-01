fba31e5268fb70201b2c88fd19dd24d1133280d7
jpanzen <jan@panzenberger.cz>
2024-12-01 22:21:00 +0100

    odstraneni netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false,
   })],
-  output: 'server',
-  adapter: netlify()
 });
